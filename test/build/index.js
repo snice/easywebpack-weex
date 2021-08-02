cb91d155becef34b1379db0bcede836c859b855e
zhuzhe <zhuzhe@findpiano.cn>
2021-08-02 09:15:58 +0800

    delete test

--- test/build/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const config = require('../webpack.config');
-const WeexWebpack = require('../../');
-const webpackConfig = WeexWebpack.getWebpackConfig(config);
-if (process.env.NODE_ENV === 'development') {
-  WeexWebpack.server(webpackConfig);
-} else {
-  WeexWebpack.build(webpackConfig);
-}
